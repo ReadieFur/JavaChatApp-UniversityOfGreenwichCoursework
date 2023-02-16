5b878403abed00f1d64c6fdc9e85eab3e5778cab
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2023-02-16 21:16:55 +0000

    Misc changes

--- src/ui/ChatUI.java
+++ src/ui/ChatUI.java
@@ -1,12 +1,17 @@
 package ui;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
 
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.SAXException;
 
 import chat_app.ChatManager;
+import chat_app.Peer;
+import chat_app.net_data.MessagePayload;
+import readiefur.console.Logger;
 import readiefur.xml_ui.Observable;
 import readiefur.xml_ui.XMLUI;
 import readiefur.xml_ui.attributes.BindingAttribute;
@@ -16,22 +21,23 @@ import readiefur.xml_ui.exceptions.InvalidXMLException;
 
 public class ChatUI extends XMLUI<Window>
 {
-    @BindingAttribute(DefaultValue = "#FFFFFF")
-    private Observable<String> backgroundColourPrimary;
+    @BindingAttribute(DefaultValue = "#FFFFFF") private Observable<String> backgroundColourPrimary;
+    @BindingAttribute(DefaultValue = "#DADADA") private Observable<String> backgroundColourSecondary;
+    @BindingAttribute(DefaultValue = "#BDBDBD") private Observable<String> backgroundColourTertiary;
+    @BindingAttribute(DefaultValue = "#000000") private Observable<String> foregroundColour;
 
-    @BindingAttribute(DefaultValue = "#DADADA")
-    private Observable<String> backgroundColourSecondary;
-
-    @BindingAttribute(DefaultValue = "#BDBDBD")
-    private Observable<String> backgroundColourTertiary;
-
-    @BindingAttribute(DefaultValue = "#000000")
-    private Observable<String> foregroundColour;
+    private final ChatManager chatManager;
 
     public ChatUI(ChatManager chatManager)
         throws IllegalArgumentException, IllegalAccessException, IOException, ParserConfigurationException, SAXException, InvalidXMLException
     {
         super();
+
+        this.chatManager = chatManager;
+
+        this.chatManager.onPeerConnected.Add(this::ChatManager_OnPeerConnected);
+        this.chatManager.onPeerDisconnected.Add(this::ChatManager_OnPeerDisconnected);
+        this.chatManager.onMessageReceived.Add(this::ChatManager_OnMessageReceived);
     }
 
     public void Show()
@@ -42,6 +48,21 @@ public class ChatUI extends XMLUI<Window>
     public void ShowDialog()
     {
         rootComponent.ShowDialog();
+    }
+
+    private void ChatManager_OnPeerConnected(UUID peerId)
+    {
+        Logger.Info("Peer connected: " + chatManager.GetPeers().get(peerId).GetUsername());
+    }
+
+    private void ChatManager_OnPeerDisconnected(UUID peerId)
+    {
+        Logger.Info("Peer disconnected: " + chatManager.GetPeers().get(peerId).GetUsername());
+    }
+
+    private void ChatManager_OnMessageReceived(MessagePayload message)
+    {
+        Logger.Info("Message received from: " + chatManager.GetPeers().get(message.GetSender()).GetUsername() + " - " + message.GetMessage());
     }
 
     @EventCallbackAttribute
