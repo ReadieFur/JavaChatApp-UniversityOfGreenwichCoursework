919f14a1e650815d9750b9f1ada9549d77a203f6
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2023-02-17 19:05:17 +0000

    Misc changes

--- src/Testing.java
+++ src/Testing.java
@@ -1,7 +1,11 @@
+import readiefur.console.ELogLevel;
+import readiefur.console.Logger;
+
 public class Testing
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        Logger.ConfigureConsole();
+        Logger.logLevel = ELogLevel.TRACE;
     }
 }
